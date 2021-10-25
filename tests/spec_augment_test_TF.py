f03e6a4654d994efdc5ca8e49dd81da3c26617e6
Jiyang Li <jiyang725@gmail.com>
2021-10-25 12:00:03 -0400

    save

--- tests/spec_augment_test_TF.py
+++ tests/spec_augment_test_TF.py
@@ -15,14 +15,15 @@
 """SpecAugment test"""
 
 import argparse
-import librosa
+# import librosa
 from SpecAugment import spec_augment_tensorflow
 import os, sys
 import numpy as np
+import pandas as pd
 # sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 parser = argparse.ArgumentParser(description='Spec Augment')
-parser.add_argument('--audio-path', default='../data/61-70968-0002.wav',
+parser.add_argument('--file-path', default='../../stft_label_vIMU/Jonathan 93_stft',
                     help='The audio file.')
 parser.add_argument('--time-warp-para', default=80,
                     help='time warp parameter W')
@@ -34,7 +35,7 @@ parser.add_argument('--masking-line-number', default=1,
                     help='masking line number')
 
 args = parser.parse_args()
-audio_path = args.audio_path
+file_path = args.file_path
 time_warping_para = args.time_warp_para
 time_masking_para = args.frequency_mask_para
 frequency_masking_para = args.time_mask_para
@@ -43,21 +44,28 @@ masking_line_number = args.masking_line_number
 if __name__ == "__main__":
 
     # Step 0 : load audio file, extract mel spectrogram
-    audio, sampling_rate = librosa.load(audio_path)
-    mel_spectrogram = librosa.feature.melspectrogram(y=audio,
-                                                     sr=sampling_rate,
-                                                     n_mels=256,
-                                                     hop_length=128,
-                                                     fmax=8000)
+    # audio, sampling_rate = librosa.load(audio_path)
+    # mel_spectrogram = librosa.feature.melspectrogram(y=audio,
+    #                                                  sr=sampling_rate,
+    #                                                  n_mels=256,
+    #                                                  hop_length=128,
+    #                                                  fmax=8000)
+    for root, dirs, files in os.walk(file_path):
+        print("Root = ", root, "dirs = ", dirs, "files = ", files)
+    for filename in files:
+        print(filename)
+        path_one = file_path + '/' + filename
+        spectrogram = pd.read_csv(path_one)
+        # mel_spectrogram = rgb2gray(mel_spectrogram_raw)
 
     # reshape spectrogram shape to [batch_size, time, frequency, 1]
-    shape = mel_spectrogram.shape
-    mel_spectrogram = np.reshape(mel_spectrogram, (-1, shape[0], shape[1], 1))
+    shape = spectrogram.shape
+    spectrogram = np.reshape(spectrogram, (-1, shape[0], shape[1], 1))
 
     # Show Raw mel-spectrogram
-    spec_augment_tensorflow.visualization_spectrogram(mel_spectrogram=mel_spectrogram,
+    spec_augment_tensorflow.visualization_spectrogram(mel_spectrogram=spectrogram,
                                                       title="Raw Mel Spectrogram")
 
     # Show time warped & masked spectrogram
-    spec_augment_tensorflow.visualization_tensor_spectrogram(mel_spectrogram=spec_augment_tensorflow.spec_augment(mel_spectrogram),
+    spec_augment_tensorflow.visualization_tensor_spectrogram(mel_spectrogram=spec_augment_tensorflow.spec_augment(spectrogram),
                                                       title="tensorflow Warped & Masked Mel Spectrogram")
